adaecd68f391ba4c882e65ee19ab06b8a50fe6f5
parzulpan <parzulpan@gmail.com>
2020-12-11 01:07:10 +0800

    03

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 一份剑指 Offer 学习笔记。
 
-## 目录
+## 章节目录
 
 * [第一章 面试的流程](ch01/README.md)
 * [第二章 面试需要的基础知识](ch02/README.md)
@@ -11,6 +11,13 @@
 * [第五章 优化时间和空间效率](ch05/README.md)
 * [第六章 面试中的各项能力](ch06/README.md)
 * [第七章 两个面试案例](ch07/README.md)
+
+## 题目目录
+
+| 题目 | 标签 | 语言 |
+| :--- | :--- | :--- |
+| [03.数组中重复的数字](https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/) | 数组 哈希表 | [Java](src/Java/03.java) [Cpp](src/Cpp/03.cpp) [Python](src/Python/03.py)|
+| | |
 
 ## 参考
 
---
+++ src/Java/03.java
@@ -0,0 +1,73 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ 
+找出数组中重复的数字。
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3
+
+限制：
+2 <= n <= 100000
+ */
+
+class Solution {
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
+        final int[] nums = new int[] { 2, 3, 1, 0, 2, 5, 3 };
+        System.out.println(solution.findRepeatNumber(nums));
+
+    }
+
+    /**
+     * 方法一：遍历数组
+     * 
+     * 遍历数组中的每个元素： * 将该元素加入集合中，判断是否加入成功 * 添加失败，则找到重复数字
+     * 
+     * 时间复杂度 O(n) 空间复杂度 O(n)
+     * 
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber(final int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int repeat = -1;
+        for (final int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+
+    /**
+     * 方法二：原地置换
+     * 
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber1(final int[] nums) {
+        int len = nums.length;
+        int temp = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                } else {
+                    temp = nums[i];
+                    nums[i] = nums[temp];
+                    nums[temp] = temp;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
